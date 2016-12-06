2de41417af2e732be6c00398666bfe03f7401b2c
zhouzhouzouzou <734487605@qq.com>
2016-12-06 21:22:58 +0800

    更新传cookies

--- 20161201_ecust_pingjiao.py
+++ 20161201_ecust_pingjiao.py
@@ -11,22 +11,19 @@ def login(no,password):
     }
     headers = {
         'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
-        'Accept-Encoding':'gzip,deflate',
         'Accept-Language':'zh-CN,zh;q=0.8',
         'User-Agent':'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36',
         'Content-Type':'application/x-www-form-urlencoded',
         'Referer':'http://pjb.ecust.edu.cn/pingce/index.php',
         'Origin':'http://pjb.ecust.edu.cn',
-        'Proxy-Connection':'keep-alive',
         'Host':'pjb.ecust.edu.cn',
-        'Upgrade-Insecure-Requests':'1',
         'Cache-Control':'max-age=0',
-        'Content-Length':'41'
     }
-    r = requests.post(url=url,data=data,headers=headers)
-    list = [r.text,r.cookies]
+    rTwo = requests.post(url=url,data=data,headers=headers,allow_redirects=False)
+    rOne = requests.post(url=url,data=data,headers=headers)
+    list = [rOne.text,rTwo.cookies]
     return list
-print(login('10150982','125018')[1])
+print(login('10150982','125018')[1].items())
 spiderFirst = re.findall('href="pg\.php+(.*?)"',login('10150982','125018')[0],re.S)
 length = len(spiderFirst)
 spiderFirstList = []
@@ -45,10 +42,17 @@ headers = {
     'Accept-Encoding':'gzip,deflate',
     'Accept-Language':'zh-CN,zh;q=0.8'
 }
+#cookies = {
+#    'PHPSESSID':'j5coiqqk6n380gq46khucp2ea5'
+#}
+cookiesRaw = login('10150982','125018')[1].items()[0]
+print(cookiesRaw)
 cookies = {
-    'PHPSESSID':'j5coiqqk6n380gq46khucp2ea5'
+    'PHPSESSID':cookiesRaw[1]
 }
+print(type(cookies))
 r = requests.get(url=url,headers=headers,cookies=cookies)
+
 fp = open('2.html','w+',encoding='utf-8')
 fp.write(r.text)
 fp.close()
