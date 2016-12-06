162332c20e7a3f0714e1022260fa9f2dd91a874d
zhouzhouzouzou <734487605@qq.com>
2016-12-06 17:08:34 +0800

    开始评教功能

--- 20161201_ecust_pingjiao.py
+++ 20161201_ecust_pingjiao.py
@@ -2,7 +2,6 @@
 import requests
 from bs4 import BeautifulSoup
 import re
-
 def login(no,password):
     url = 'http://pjb.ecust.edu.cn/pingce/login.php'
     data = {
@@ -24,11 +23,32 @@ def login(no,password):
         'Cache-Control':'max-age=0',
         'Content-Length':'41'
     }
-    r = requests.post(url,data=data,headers=headers)
-    return r.text
-spiderFirst = re.findall('href="pg\.php+(.*?)"',login('10150982','125018'),re.S)
+    r = requests.post(url=url,data=data,headers=headers)
+    list = [r.text,r.cookies]
+    return list
+print(login('10150982','125018')[1])
+spiderFirst = re.findall('href="pg\.php+(.*?)"',login('10150982','125018')[0],re.S)
 length = len(spiderFirst)
 spiderFirstList = []
 for i in range(0,length-1):
     spiderFirstList.append('http://pjb.ecust.edu.cn/pingce/pg.php'+spiderFirst[i])
-    print(spiderFirstList)
+print(spiderFirstList)
+
+url = str(spiderFirstList[0])
+headers = {
+    'Host':'pjb.ecust.edu.cn',
+    'Connection':'keep-alive',
+    'Cache-Control':'max-age=0',
+    'Upgrade-Insecure-Requests':'1',
+    'User-Agent':'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36',
+    'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+    'Accept-Encoding':'gzip,deflate',
+    'Accept-Language':'zh-CN,zh;q=0.8'
+}
+cookies = {
+    'PHPSESSID':'j5coiqqk6n380gq46khucp2ea5'
+}
+r = requests.get(url=url,headers=headers,cookies=cookies)
+fp = open('2.html','w+',encoding='utf-8')
+fp.write(r.text)
+fp.close()
